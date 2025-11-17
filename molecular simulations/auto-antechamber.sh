46dfa565103980fa00019e985240b7608a60ffb9
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2025-11-17 12:36:22 -0500

    Change path for ligand PDB files in script

--- molecular simulations/auto-antechamber.sh
+++ molecular simulations/auto-antechamber.sh
@@ -8,7 +8,7 @@ charge_cal=bcc
 iFORM=pdb
 oFORM=mol2
 out_dir=./
-for f in ../ligs/*.pdb; do
+for f in ./ligs/*.pdb; do
     b=`basename $f .pdb`
     echo Running ANTECHAMBER $b
     $ANTE_CHAMBER -i $f -fi $iFORM -o $out_dir/${b}.$oFORM -fo $oFORM -nc $lig_charges -c $charge_cal -s $verbose_opt
